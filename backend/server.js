352f836e2ab8b49f72ea9be4de63bfab70897957
Geraldo Grell <85137475+Jikjii@users.noreply.github.com>
2023-02-02 18:31:29 -0500

    feat: server file added

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
-const cors = require('cors');
+const cors = require("cors");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const { logEvents } = require("./middleware/logger");
 const corsOptions = require("./config/corsOption");
+
+console.log(process.env.NODE_ENV);
+
+connectDB();
 
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -34,4 +42,15 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
